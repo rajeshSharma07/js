8c2320c0bc2e27c2e9b8124daca0ef8f41be5995
Rajesh Sharma <121995920+rajeshSharma07@users.noreply.github.com>
2025-02-03 15:12:16 +0000

    variable and  datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+ "use strict";// treat all js code newer version
+ 
+//  alert(3 + 3) we are using node.js not browser, so it is showing alert is not defined
+
+let name  = "Rajesh "    
+let age = 18             
+let isLoggedIn = false  
+
+
+//Number =>2 to pwoer 53
+//Bigint
+//string=>""
+//boolean => true/false
+//null => it is standalone value
+// undefined 
+//symbol => it used for uniqueness
+
+
+
+//object
+
+console.log(typeof "hitesh")   // it's output is string
+
+console.log(typeof age)        // it's output is number
+
+console.log(typeof null)       // it's output is object
+
+console.log(typeof undefined)  // it's output is undefined
+
