e366d32346882c5ffad23c3349b7ec76ed24d577
Rajesh Sharma <121995920+rajeshSharma07@users.noreply.github.com>
2025-02-04 14:40:40 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,3 +23,42 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// ****************operations ******************
+
+let value = 3 
+let negValue = -value       //output is -3
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);        // otpt=8
+console.log(2%3);
+console.log(2/3);
+ 
+let str1 = "Rajesh"
+let str2 = " Sharma"
+let str3 = str1 + str2
+console.log(str3);        // otpt is Rajesh Sharma
+
+console.log("1" + 2);        //output is 12
+console.log(1 + "2");        //output is 12
+console.log("1" + 2 + 2);    // output is 122
+console.log(1 + 2 + "2");    // output is 32
+
+
+// console.log(true);        // output is true
+// console.log(+true);       // output is 1
+// console.log(true+);       //give error
+// console.log(+"");         // output is  0 
+
+let gameCounter=100
+gameCounter++;               //101
+console.log(gameCounter);
+
+
+
+
+
