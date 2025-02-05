e70f2c1cd6642defff9cc96f0cdaee8d5204e5fe
Rajesh Sharma <121995920+rajeshSharma07@users.noreply.github.com>
2025-02-05 15:32:03 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,22 @@
+// how to push data in memory and how access on this basis datatypes are two types.
+//# primitive-  it is call by value
+//7 types-: String, Number, Boolean, null, undefined, Symbol, BigInt.
+
+
+
+
+
+//refrence(Non Primitive)
+// Array, Object, Functions
+
+const heros = [ "saktiman", "prabhas", "puspa"];
+let obj = {
+    name :"rajesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("hello my name is Rajesh Sharma");
+}
+
+console.log(typeof heros);         //  all non premitive data type value is  "object function".
