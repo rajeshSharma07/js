db91056528cd0b59065af6c97d35d369aa279ce6
Rajesh Sharma <121995920+rajeshSharma07@users.noreply.github.com>
2025-02-05 14:59:26 +0000

    comparison using operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2==1);       all output is showing true/false
+//console.log(2!=1);
+
+console.log("2" >1);
+console.log("02">1);     //both output is true 
+
+console.log(null>0);
+console.log(null==0);     // for in three cases null is converted in 0, and according to give output
+console.log(null>=0);
+
+
+console.log(undefined=0);
+console.log(undefined>0);       // in these 3 cases undefined is converted in 0, and according to give ouput. 
+console.log(undefined<0);
+
+
+// ===
+console.log("2"===2);   // it is not equal
+
+
+
