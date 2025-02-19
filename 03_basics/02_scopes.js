330df2e39041d9a560bf64b6bd3c17b5183ecf5b
Rajesh Sharma <121995920+rajeshSharma07@users.noreply.github.com>
2025-02-19 17:17:39 +0000

    scope level and hosting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -16,3 +16,42 @@ let a = 300
    console.log(a);
   // console.log(b);       // it showing  a and b is not defind bu c is giving ouput 30
   // console.log(c);
+
+
+  function one(){
+    const username = "Rajesh"
+    function two(){
+         const website = "youtube"
+         console.log(username);
+    }
+    //console.log(website);
+
+   two()
+  }
+  //one()
+
+  if (true){
+    const username = "Rajesh"
+    if (username == "Rajesh"){
+        const website = "youtube"
+        console.log(username + website);
+    }
+   // console.log(website);
+  }                            //both commented console give no output because it is defind out of the function
+  //console.log(username);
+
+
+  //*************** interesting ************** */
+  console.log(addone(5))
+  function addone (num){
+    return num +1
+  }
+  
+
+
+  addTwo(5)
+ // const addTwo = (nu any  
+  const addTwo = function(num){
+    return num +2
+  }
+  //addTwo(5)
