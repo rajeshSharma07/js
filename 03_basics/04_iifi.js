3a6ea3dbafad02be48379a656ccfa958199d340c
Rajesh Sharma <121995920+rajeshSharma07@users.noreply.github.com>
2025-02-20 17:46:55 +0000

    This is IIFI Function used to escape from global scope pollution.

---
+++ 03_basics/04_iifi.js
@@ -0,0 +1,9 @@
+(function chai(){      // it is and named IIFI because it have name of funcion
+    console.log('DB CONNECTEC');
+})();
+
+// suing arow  function inside iffi
+
+((name)=>{        // it is unname IIFI because it is not have function name.
+ console.log('DB CONNECTED TWO ${name}');
+})('Rajesh')
