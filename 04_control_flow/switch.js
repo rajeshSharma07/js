0465dd4a9b546e0c29c8bdb760c19d8b2c64ea0e
Rajesh Sharma <121995920+rajeshSharma07@users.noreply.github.com>
2025-02-23 18:24:41 +0000

    we learn Switch cases.

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,33 @@
+// switch(key){
+//     case value:
+
+//         Break;
+
+//         default:
+//             break;
+
+// }
+
+
+
+
+const month = 3 
+
+switch( month){
+    case 1:
+        console.log("januray");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+        default:
+            console.log("default case is match");
+            break;
+
+}
