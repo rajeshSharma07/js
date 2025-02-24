59e1ab82efa1c208b0a48a99aa8555a3782aa747
Rajesh Sharma <121995920+rajeshSharma07@users.noreply.github.com>
2025-02-24 17:17:10 +0000

    while and do while loop

---
+++ 05_iterator/two.js
@@ -0,0 +1,22 @@
+// let index = 0
+
+// while(index<=10){
+// console.log("value of index is ", index);
+// index +=2
+// }
+
+
+let arr=0
+let array =["rajesh", "rakesh","nilesh"]
+while(arr<array.length){
+    console.log('value is',array[arr]);
+    arr+=1
+}
+
+
+
+let score = 11
+do{
+console.log('score is ',score);
+score++
+}while(score <=10);
