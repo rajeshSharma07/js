93895b54f3893ee0f5849ef38935433a232329b6
Rajesh Sharma <121995920+rajeshSharma07@users.noreply.github.com>
2025-02-25 18:49:34 +0000

    for in loop

---
+++ 05_iterator/four.js
@@ -0,0 +1,17 @@
+const myObject = {
+    js:'javascript',
+    cpp: 'c++',
+    rb:'ruby',
+    swift: 'swift by apple'
+}
+ 
+for (const key in myObject){
+    console.log(key, 'wortcut  is for',myObject[key]);
+}
+
+
+const  programming = [ 'js','rb','py','java','cpp']
+  
+for(const key in programming){
+    console.log(programming[key]);
+}
