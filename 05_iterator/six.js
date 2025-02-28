ce2fad576a63b472310d142123289a81cdde0459
Rajesh Sharma <121995920+rajeshSharma07@users.noreply.github.com>
2025-02-28 12:18:47 +0000

    filter function

--- 05_iterator/six.js
+++ 05_iterator/six.js
@@ -1,6 +1,23 @@
-const coding = ["js", "ruby", "java","python", "cpp"]
- const values = coding.forEach((item) =>{
-    console.log(values);
-    //return item;
- })
- //console.log(value);
+// const coding = ["js", "ruby", "java","python", "cpp"]
+//  const values = coding.forEach((item) =>{
+//     console.log(values);
+//     //return item;
+//  })
+//  //console.log(value);
+
+
+
+  const mynums = [1,2,3,4,5,6,7,8,9,10]
+//  let newnums =mynums.filter((num) => num>4)
+//  console.log(newnums);
+
+
+
+ const newnums = []
+
+  mynums.forEach ((num)=>{
+   if (num > 4){
+     newnums.push(num)
+   }
+  });
+  console.log(newnums);
